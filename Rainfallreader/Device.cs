dd6d7d468b8378fa811facec95a26fb9b36ee184
Martin Hill <he134252@health.wa.gov.au>
2023-11-04 10:54:31 +0800

    Filled basic container class Device

--- Rainfallreader/Device.cs
+++ Rainfallreader/Device.cs
@@ -2,5 +2,42 @@
 {
     internal class Device
     {
+        private string id;
+
+        private string name;
+
+        private string location;
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public string Location
+        {
+            get
+            {
+                return location;
+            }
+        }
+
+        public Device(string id, string name, string location)
+        { 
+            // Explicit assignment in case we want to do validation or conversion later.
+           this.id = id;
+           this.name = name;
+           this.location = location;
+        }
     }
 }
